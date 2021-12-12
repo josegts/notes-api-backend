3b747ee9c34ec79a9548a14ae026b7c39ccc22fe
Jose Luis Gutierrez Carcausto <jocgts@gmail.com>
2021-12-12 00:16:05 -0500

    add heroku needed config

--- index.js
+++ index.js
@@ -71,7 +71,9 @@ app.use((request, response) => {
   console.log('Entre aquii....');
   response.status(404).end();
 });
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
